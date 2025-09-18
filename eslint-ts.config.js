2e1ceb196e0157b03c8dc975bc6fba2a5ed26042
Ethan Arrowood <ethan@arrowood.dev>
2025-09-18 16:42:48 -0600

    i dont know if this is correct

--- eslint-ts.config.js
+++ eslint-ts.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'eslint/config';
 import harperBaseConfig from './eslint.config.js';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig(harperBaseConfig, tseslint.configs.recommended);
